81a8863b6d63bee34bcc886a86353af1963c6f0b
Carlos Anderson <carlosjanderson@gmail.com>
2016-12-19 09:55:06 -0500

    Switch index names and improve range

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,8 +1,8 @@
 def insertion_sort(a):
-    for j in range(len(a)):
-        key = a[j]
-        i = j - 1
-        while i >= 0 and a[i] > key:
-            a[i + 1] = a[i]
-            i = i - 1
-        a[i + 1] = key
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
